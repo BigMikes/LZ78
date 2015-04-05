3968239e03b9e89e4f78548a49f733e22da23989
BigMikes <giulio.micheloni@gmail.com>
2015-04-05 14:52:03 +0200

    Options Handler improved

--- Progetto LZ78/lz78.c
+++ Progetto LZ78/lz78.c
@@ -1,47 +1,67 @@
 #include "lz78.h"
 
+
+
+//Display a help message
+void help(){
+	char* options_msg = "Usege: lz78 \n\
+	 -c ..................: compressor mode\n\
+	 -d ..................: decompressor mode\n\
+	 -i \"<filename>\"......: input file name\n\
+         -o \"<filename>\"......: output file name\n\
+        [-v ].................: verbose\n\
+         -s \"<size>\"..........: dictionary size \n";
+        
+	printf("%s", options_msg);		
+}
+
+
+
 int main(int argc, char* argv[]){
 /*--------------Variables---------------*/
 	char opt;
 	const char* opt_string = "cdi:o:s:v";
-	int num_opt_min = 4;
-	int num_opt_max = 5;
-	char* options_msg = "Use \"lz78 ['-c' compress | '-d' decompress] ['-i' <input_file>] ['-o' <output_file>] ['-v' verbose] ['-s' <dictionary_size>]\"\n";		
+	char* input_file;
+	char* output_file;
+	int dict_size = 0;
+	
 /*--------------Options Handle----------*/
-	if(argc < num_opt_min || argc > num_opt_max){
-		printf("%s", options_msg);
-		exit(-1);
-	}
+
 	while((opt = getopt(argc, argv, opt_string)) != -1){
 		switch(opt){
 			// Option '-c' for compressor mode
 			case 'c':
-			
+				
 				break;
 			// Option '-d' for decompressor mode
 			case 'd':
-			
+				
 				break;
 			// Option -i <filename> for set the input file name
 			case 'i':
-			
+				input_file = strdup(optarg);
 				break;
 			// Option -o <filename> for set the output file name
 			case 'o':
-			
+				output_file = strdup(optarg);
 				break;
 			// Option -s <dictionary_size> for set the dictionary size
 			case 's':
-			
+				dict_size = atoi(optarg);
+				//------------------------------------------------------------gestire errore di size <= 0 oppure troppo grande
 				break;
 			// Option -v for verbose mode
 			case 'v':
-			
+	
 				break;
 			default:
-				printf("%s", options_msg);
-				exit(-1);
+				help();
+				return 0;
 		}	
 	}
+	//-------------------------------------------------------------------------------------gestire errore opzioni obbligatorie mancanti
+	printf("Input file: %s\n", input_file);
+	printf("Output file: %s\n", output_file);
+	printf("Dictionary size: %i\n", dict_size);
 	return 0;
 }
