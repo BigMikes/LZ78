ae42b0f7e938a378aba9eed3bdcdf8fc46cf504d
BigMikes <giulio.micheloni@gmail.com>
2015-04-09 17:14:04 +0200

    Update hash table and corrected a bug
    Corrected the use of RNG and updated the hash table entry to a new version.

--- Progetto LZ78/hash_table.c
+++ Progetto LZ78/hash_table.c
@@ -1,18 +1,15 @@
 #include "hash_table.h"
 
-//Element that represents a child in the tree
-struct children_s{
-	int node_id;
-	children_t *next;	
-};
+//Define a type for the hash value. Code maintenance purpose. 
+typedef uint32_t hashint;
+
 
 
 //Single entry of the hash table
 struct entry_s{
-	char *key;				//The key of this record, is the hash value computed from 'node_id' and 'value'
-	int node_id;				//Node identifier
+	int father_id;				//Father node identifier
 	char value;				//The associated character to the node 
-	children_t *children;			//Children list of this node
+	int node_id;				//Node identifier
 	//PUNTATORE AL PADRE?
 };
 
@@ -21,13 +18,26 @@ struct entry_s{
 struct hashtable_s{
 	int size;				//Real dimention of hash table
 	int num_records;			//Number of present records in the table
+	hashint hash_start_value;		//It will contain the Benrstein's magic number 5381 mixed with a random number
 	entry_t **table;			//It will be an array of pointers to entry_t elements, the reason to keep memory to the minimum required
 };
+
+
+
+//Brenstein's hash function
+hashint hash(hashtable_t* ht, unsigned char* c, int dim){
+	hashint result = ht->hash_start_value;
+	int i;
+	for(i = 0; i < dim; i++)
+		result = ((result << 5) + result) + c[i];		// h_i = |h_i-1 * 33 + c|
+	return result;
+}
+
 
 //Create the hash table with a given size
 hashtable_t* create_hash_table(int size){
 	hashtable_t *ret = NULL;
-	
+	int seed; 
 	if(size <= 0)
 		return NULL;
 	
@@ -45,46 +55,35 @@ hashtable_t* create_hash_table(int size){
 	
 	ret->size = size;
 	ret->num_records = 0;
+	ret->hash_start_value = 5381;
+	//Seed the random number generator (used in hash value generation)
+	srand(time(NULL));			
 	
-	srand(time(NULL));			//Seed the random number generator (used in hash value generation)
+	//In order to keep the hash table resistent from a possible collision attack
+	seed = rand();
+	ret->hash_start_value = hash(ret, (unsigned char*) &seed, sizeof(seed));
 	
 	return ret;
 	
 }
 
-//Brenstein's hash function
-uint32_t hash(uint32_t hash_value, char c){
-	uint32_t result;
-	result = ((hash_value << 5) + hash_value) + c;
-	return result;
-}
 
 
-//Given the key <Node_id, Symbol> return the hash value
-uint32_t key_generation(hashtable_t* ht, int node_id, char symbol){
-	uint32_t hash_value = 5381;
-	uint32_t hash_seed = rand();
-	int i;
-	char app;
+//Given the key: <Father_id, Symbol> return the hash value
+hashint key_generation(hashtable_t* ht, int father_id, char symbol){	
+	hashint hash_value;
+	//The buffer that will contain the father_id concatenated with symbol 
+	unsigned char* to_hash;
+	int dim = sizeof(father_id) + sizeof(symbol);
 	
-	//generate the h_1 value
-	for(i = 0; i < sizeof(hash_seed); i++){
-		app = (hash_seed >> (i * 8)) & 255;
-		hash_value = hash(hash_value, app);
-	}
+	to_hash = malloc(dim);
 	
-	//hash node_id
-	for(i = 0; i < sizeof(node_id); i++){
-		app = (node_id >> (i * 8)) & 255;
-		hash_value = hash(hash_value, app);
-	}
+	*to_hash = (father_id << (sizeof(symbol) * 8)) | symbol;
 	
-	//hash symbol
-	hash_value = hash(hash_value, symbol);
+	//Hash
+	hash_value = hash(ht, to_hash, dim); 	
+	
+	free(to_hash);
 	
 	return hash_value % ht->size;
 }
-
-
-
-
