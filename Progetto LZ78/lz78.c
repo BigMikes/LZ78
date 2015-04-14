a0fbc19cec6449f25eb686a8da75202685dd85c8
luigiDB <spada.elfica@gmail.com>
2015-04-15 00:39:22 +0200

    Finita la gestione dei parametri compresa la gestione degli errori.

--- Progetto LZ78/lz78.c
+++ Progetto LZ78/lz78.c
@@ -1,16 +1,28 @@
 #include "lz78.h"
 
-
+struct parameters{
+	//mode 1->compressor
+	//mode 2->decompressor
+	int mode; 
+	char* input_file;
+	char* output_file;
+	int verbose;
+	int dict_size;
+	int i_set;
+	int o_set;
+	int s_set;
+};
 
 //Display a help message
 void help(){
-	char* options_msg = "Usege: lz78 \n\
-	 -c ..................: compressor mode\n\
-	 -d ..................: decompressor mode\n\
-	 -i \"<filename>\"......: input file name\n\
-         -o \"<filename>\"......: output file name\n\
-        [-v ].................: verbose\n\
-         -s \"<size>\"..........: dictionary size \n";
+	char* options_msg = "Usage: lz78 \n\
+		-c ....................: compressor mode\n\
+		-d ....................: decompressor mode\n\
+		-i \"<filename>\"........: input file name\n\
+		-o \"<filename>\"........: output file name\n\
+		-v ....................: verbose\n\
+		-s \"<size>\"............: dictionary size \n\
+		-h ....................: this helper";
         
 	printf("%s", options_msg);		
 }
@@ -18,54 +30,101 @@ void help(){
 
 
 int main(int argc, char* argv[]){
-/*--------------Variables---------------*/
+/*--------------Variables---------------------------------------------*/
 	char opt;
-	const char* opt_string = "cdi:o:s:v";
-	char* input_file = NULL;
-	char* output_file = NULL;
-	int dict_size = 0;
-	
-/*--------------Options Handle----------*/
+	struct parameters param;
 
-	while((opt = getopt(argc, argv, opt_string)) != -1){
+/*--------------Inizilize default parameters struct-------------------*/
+	param.mode = 0;
+	param.verbose = 0;
+	param.dict_size = 0;
+	param.i_set = 0;
+	param.o_set = 0;
+	param.s_set = 0;
+
+/*--------------Options Handle----------------------------------------*/
+	while((opt = getopt(argc, argv, "cdi:o:s:vh")) != -1){
 		switch(opt){
 			// Option '-c' for compressor mode
 			case 'c':
-				
+				if(param.mode!=0){
+					printf("It's not possible to code and decode");
+					return 0;
+				}
+				param.mode = 1;
 				break;
 			// Option '-d' for decompressor mode
 			case 'd':
-				
+				if(param.mode!=0){
+					printf("It's not possible to code and decode");
+					return 0;
+				}
+				param.mode = 2;
 				break;
 			// Option -i <filename> for set the input file name
 			case 'i':
-				input_file = strdup(optarg);
+				param.i_set = 1;
+				param.input_file = strdup(optarg);
 				break;
 			// Option -o <filename> for set the output file name
 			case 'o':
-				output_file = strdup(optarg);
+				param.o_set = 1;
+				param.output_file = strdup(optarg);
 				break;
 			// Option -s <dictionary_size> for set the dictionary size
 			case 's':
-				dict_size = atoi(optarg);
-				//------------------------------------------------------------gestire errore di size <= 0 oppure troppo grande
+				param.s_set = 1;
+				param.dict_size = atoi(optarg);
 				break;
 			// Option -v for verbose mode
 			case 'v':
-	
+				param.verbose = 1;
 				break;
+			case 'h':
 			default:
 				help();
 				return 0;
 		}	
 	}
-	//-------------------------------------------------------------------------------------gestire errore opzioni obbligatorie mancanti
-	printf("Input file: %s\n", input_file);
-	printf("Output file: %s\n", output_file);
-	printf("Dictionary size: %i\n", dict_size);
+
+/*-------------gestione errori----------------------------------------*/
+	//file di input necessario
+	//il file è sempre accompagnato dall'estensione per cui è sempre di almeno 5 caratteri
+	if(!param.i_set || strlen(param.input_file)==2){	
+		printf("needed input file\n");
+		return 0;
+	}
+	
+	//se il file di output non viene fornito di default è output.txt
+	if(!param.o_set || strlen(param.output_file)==2){
+		param.output_file = (char*)malloc(sizeof("output.txt")+1);
+		param.output_file = "output.txt";
+	}
+	
+	//controllo sulla dimensione
+	if(!param.s_set){
+		param.dict_size = (int)UINT16_MAX;
+	} else {
+		if(param.dict_size<0){						//non penso possa capitare un numero upperbound????
+			printf("out of bound value\n");
+		}
+	}
+	
+	//controllo del modo di funzionamento
+	if(param.mode==0){
+		printf("Must be selected one functioning mode");
+		return 0;
+	}
+	
+	//se in modo decompressore s viene semplicemente ignorato
+/*--------------------------------------------------------------------*/	
+	
+	printf("Input file: %s\n", param.input_file);
+	printf("Output file: %s\n", param.output_file);
+	printf("Dictionary size: %i\n", param.dict_size);
 	
 	/*_____________________________TESTING HASH TABLE____________________________*/
-	
+	/*
 	hashtable_t* hashtable;
 	hashtable = create_hash_table(100);
 	if(hashtable == NULL){
@@ -111,6 +170,6 @@ int main(int argc, char* argv[]){
 		printf("The node was found, node id = %i\n", node_id); 	
 	else
 		printf("The node wasn't found, node id = %i\n", node_id);
-	
+	*/
 	return 0;
 }
